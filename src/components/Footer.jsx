5d2eb4e0b67feaf036575e4504277f4d718e5d24
PureZinc <139257470+PureZinc@users.noreply.github.com>
2024-09-09 19:58:50 -0400

    Added Service.js to keep business data organized

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { COMPANY_LINKS as links } from '../service'
 
 export default function Footer() {
   return (
@@ -7,9 +8,9 @@ export default function Footer() {
         <div className="flex justify-between items-center mt-5">
           <p className="text-sm">&copy; {new Date().getFullYear()} Nov8 Devs</p>
           <div className="flex space-x-4">
-            <a href="#" className="text-sm hover:text-gray-400">Instagram</a>
-            <a href="#" className="text-sm hover:text-gray-400">Twitter</a>
-            <a href="#" className="text-sm hover:text-gray-400">Contact Us</a>
+            <a href={links.instagram} className="text-sm hover:text-gray-400">Instagram</a>
+            <a href={links.twitter} className="text-sm hover:text-gray-400">Twitter</a>
+            <a href={links.github} className="text-sm hover:text-gray-400">GitHub</a>
           </div>
         </div>
       </div>
---
+++ src/service.js
@@ -0,0 +1,10 @@
+export const BASE_API_URL = "https://backendurl.api"
+
+// Edit all links with #
+export const COMPANY_LINKS = {
+    instagram: "https://www.instagram.com/",
+    twitter: "https://twitter.com/",
+    github: "https://github.com/nov8devs/stoistrom",
+    privacyPolicy: "#",
+    companyDocs: "#"
+}
